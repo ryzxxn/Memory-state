b9c9227378d251d61e845025c3cf0237692f8504
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2024-06-05 02:20:02 +0530

    asdas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+const BroadcastChannel = typeof window !== 'undefined' ? window.BroadcastChannel : null;
+
 class MemoryState {
   constructor() {
     if (!MemoryState.instance) {
       this.state = {};
+      this.listeners = {};
+      if (BroadcastChannel) {
+        this.channel = new BroadcastChannel('memory-state-channel');
+        this.channel.addEventListener('message', (event) => {
+          const { key, value } = event.data;
+          this.setState(key, value);
+        });
+      }
       MemoryState.instance = this;
     }
     return MemoryState.instance;
@@ -9,6 +19,12 @@ class MemoryState {
 
   setState(key, value) {
     this.state[key] = value;
+    if (this.channel) {
+      this.channel.postMessage({ key, value });
+    }
+    if (this.listeners[key]) {
+      this.listeners[key].forEach((callback) => callback(value));
+    }
   }
 
   getState(key) {
@@ -17,10 +33,26 @@ class MemoryState {
 
   clearState(key) {
     delete this.state[key];
+    if (this.listeners[key]) {
+      this.listeners[key].forEach((callback) => callback(null));
+    }
   }
 
   clearAll() {
     this.state = {};
+    Object.keys(this.listeners).forEach((key) => {
+      this.listeners[key].forEach((callback) => callback(null));
+    });
+  }
+
+  onChange(key, callback) {
+    if (!this.listeners[key]) {
+      this.listeners[key] = [];
+    }
+    this.listeners[key].push(callback);
+    return () => {
+      this.listeners[key] = this.listeners[key].filter((cb) => cb !== callback);
+    };
   }
 }
 
